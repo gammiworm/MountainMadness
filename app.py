e1d263d2a2e38668b3078587c09750c38ca77aff
Minseo Kim <charlottekimms@gmail.com>
2025-03-08 18:05:59 -0800

    Separated filter generation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 layers = tf.keras.layers
 import matplotlib.pyplot as plt
 import numpy as np
+from iteration.py import newFilter
 
 # Load CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -14,28 +15,37 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 class_names = ['Tom', 'Jerry', 'Both', 'Neither']
 
 # Build CNN model
-model = keras.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.Flatten(),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(10, activation='softmax')
-])
+array = [
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(10, activation='softmax')
+        ]
 
-# Compile the model
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+while True:
+    model = keras.Sequential(array)
 
-# Train the model
-history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+    # Compile the model
+    model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
 
-# Evaluate the model
-test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
-print(f'\nTest accuracy: {test_acc:.4f}')
+    # Train the model
+    history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+    # Evaluate the model
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+    print(f'\nTest accuracy: {test_acc:.4f}')
+
+    filter = newFilter()
+    index = len(array) - 4
+    array.insert(index, filter)
+
+
+    break
+
 
 # Plot training history
 plt.plot(history.history['accuracy'], label='Accuracy')
