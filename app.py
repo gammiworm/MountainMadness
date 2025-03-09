e2bf69ad61c7400869d9c3331a6748c2ba34411e
Mason <masonliang2@hotmail.com>
2025-03-08 19:53:07 -0800

    minor changes

--- app.py
+++ app.py
@@ -7,21 +7,19 @@ from iteration.py import newFilter
 
 # Load CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-
 # Normalize pixel values to be between 0 and 1
 x_train, x_test = x_train / 255.0, x_test / 255.0
-
 # Define class names
-class_names = ['Tom', 'Jerry', 'Both', 'Neither']
+class_names = ['tom', 'jerry', 'both', 'neither']
 
 # Build CNN model
 array = [
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
-        # layers.MaxPooling2D((2, 2)),
-        # layers.Conv2D(64, (3, 3), activation='relu'),
+        #layers.MaxPooling2D((2, 2)),
+        #layers.Conv2D(64, (3, 3), activation='relu'),
         layers.Flatten(),
         # layers.Dense(64, activation='relu'),
-        layers.Dense(10, activation='softmax')
+        layers.Dense(4, activation='softmax')
         ]
 
 while True:
